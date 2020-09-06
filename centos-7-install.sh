3e63a2dadfb32d9680e91a1408ab9e253a89a496
itzsrikanth <itzsrikanth@users.noreply.github.com>
2020-09-06 07:36:59 +0530

    k8s master server for centos 7

---
+++ centos-7-install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+swapoff -a
+setenforce 0
+# disable /etc/fstab
+# disable /etc/selinux/config
+
+sudo yum check-update
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum -y install docker
+sudo systemctl enable docker
+sudo systemctl start docker
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+V=1.15.3
+yum install -y kubelet-$V kubeadm-$V kubectl-$V --disableexcludes=kubernetes
+systemctl start kubelet
+systemctl enable kubelet
+
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+kubeadm init --kubernetes-version "v1.15.3" --pod-network-cidr 10.244.0.0/16
+
+mkdir ~/.kube
+sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+sudo chown $(id -u):$(id -g) ~/.kube/config
